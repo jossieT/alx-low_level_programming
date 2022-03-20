50c07da3839d360881fedea3f8eeda90e81d3065
root <joseteshe2017@gmail.com>
2022-03-20 01:26:31 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - debugging example
+ * Return: 0
+ */
+int main(void)
+{
+	char *holberton = "_putchar";
+
+	while (*holberton)
+	{
+		_putchar(*holberton);
+		holberton++;
+	}
+	_putchar('\n');
+	return (0);
+}
