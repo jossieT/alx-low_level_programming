dcf41d586836d24b4dd53f55eb53c4899cbbf42d
root <joseteshe2017@gmail.com>
2022-03-20 03:22:51 -0700

    commited

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,16 @@ void print_to_98(int n)
 	if(n <= 98)
     	{
 	       	for(n ; n <= 98; n++)
-    	{
-        printf("%d ,", n);
-    	}
+		{
+        		printf("%d ,", n);
+		}
     	}
     	else
-    	{
+	{
         	for(n ;n >= 98; n--)
-    	{
-        printf("%d ,", n);
-    	}
+    		{
+        		printf("%d ,", n);
+    		}
     	}
     	_putchar('\n');
 }
